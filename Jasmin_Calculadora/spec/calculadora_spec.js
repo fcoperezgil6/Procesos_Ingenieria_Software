cfeebbc5c7723220db52f1fee713d82786d5a461
Francisco Javier Pérez Gil <franciscovalde6@gmail.com>
2014-11-04 08:57:40 +0100

    Completado Jasmin Calculadora

--- Jasmin_Calculadora/spec/calculadora_spec.js
+++ Jasmin_Calculadora/spec/calculadora_spec.js
@@ -1,6 +1,51 @@
 describe("Calculadora",function(){
 	var cal = new Calc();
-				it("Debería sumar números",function(){					
-					expect(cal.suma(5)).toEqual(5);
-				})
-	})
+	beforeEach(function() {
+		cal.actual=0;
+	});
+	/*
+	it("debería almacenar el valor actual en todo momento",function(){
+				expect(cal.actual).toBeDefined(); //debe existir la variable
+				expect(cal.actual).toEqual(0);	 //debe tener valor inicial 0
+			});
+
+	it("Debería sumar números",function(){
+		expect(cal.suma(5)).toEqual(5);
+		expect(cal.suma(5)).toEqual(10);
+	});
+
+	it("Sumar cualquier número", function(){
+		expect(cal.suma(1,2,3)).toEqual(6);
+		expect(cal.suma(1,2,3,4)).toEqual(16);
+	});
+*/
+
+	describe("Al sumar números",function(){
+		it("debería almacenar el valor actual en todo momento",function(){
+			expect(cal.actual).toBeDefined(); //debe existir la variable
+			expect(cal.actual).toEqual(0);	 //debe tener valor inicial 0
+		});
+
+		it("Debería sumar números",function(){	
+			expect(cal.suma(5)).toEqual(5);
+			expect(cal.suma(5)).toEqual(10);
+		});
+
+		it("sumar cualquier conjunto de números",function(){
+			expect(cal.suma(1,2,3)).toEqual(6);
+		});
+
+	});
+
+	describe("Al restar números", function(){
+		it("Debería restar cualquier conjunto de números", function(){
+			expect(cal.resta(5)).toEqual(-5);
+		});
+	});
+
+	it("debería resetear el valor de la calculadora",function(){
+		cal.actual=20;
+			cal.reset();
+			expect(cal.actual).toEqual(0);
+	});
+})
--- Jasmin_Calculadora/src/calculadora.js
+++ Jasmin_Calculadora/src/calculadora.js
@@ -1,5 +1,35 @@
 var Calc=function(){
-					this.suma=function(num){
-						return 0+num;
-					};
-				};
+	this.actual = 0;
+	
+	this.suma=function(num){
+		this.actual += num;
+		return this.actual;
+	};
+
+	this.suma=function(){
+		var sum = this.actual;
+		for(var i=0; i<arguments.length;i++){
+			sum += arguments[i];
+		}
+		this.actual = sum;
+		return this.actual;
+	};
+
+	this.resta=function(num){
+		this.actual -= num;
+		return this.actual;
+	};
+
+	this.resta=function(){
+		var res = this.actual;
+		for(var i=0; i<arguments.length;i++){
+			res -= arguments[i];
+		}
+		this.actual = res;
+		return this.actual;
+	};
+
+	this.reset=function(){
+		this.actual=0;
+	}
+};
